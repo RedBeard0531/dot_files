ac96f9d7beb8c0d468a2636759c4fe2c1186d9f8
Mathias Stearn <redbeard0531@gmail.com>
2010-07-16 19:37:56 -0400

    more plugins

--- runme.sh
+++ runme.sh
@@ -18,11 +18,12 @@
 #git clone git://github.com/tpope/vim-repeat.git
 #git clone git://github.com/tpope/vim-surround.git
 #svn checkout http://conque.googlecode.com/svn/trunk/ conque
+#git clone http://github.com/tpope/vim-fugitive.git
+#git clone http://github.com/pangloss/vim-javascript.git js-indent
 #git clone git://git.wincent.com/command-t.git
 
 #cd command-t/ruby/command-t
 #ruby extconf.rb
 #make
 #cd ../../..
-
 
